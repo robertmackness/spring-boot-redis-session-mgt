9a05269a2aa59fd9f6802d6ec617a014ea467976
Robert Mackness <rob.mackness@qikserve.com>
2018-05-19 11:43:49 +0100

    Spring Initializr Output

---
+++ src/main/java/com/mackness/springboot/session/redis/test/TestApplication.java
@@ -0,0 +1,12 @@
+package com.mackness.springboot.session.redis.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestApplication.class, args);
+	}
+}
